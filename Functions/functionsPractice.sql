c3791aa49640f546a597a955208dc248f201a454
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2025-04-02 22:58:40 +0530

    Added FUnction queries till mathematical functions

---
+++ Functions/functionsPractice.sql
@@ -0,0 +1,63 @@
+Select * from BolMovies;
+
+
+-- ALTER TABLE BolMovies Rename COLUMN "Movie Name" To "movie";
+-- ALTER TABLE BolMovies Rename COLUMN "Lead Star" To "Actor";
+-- ALTER TABLE BolMovies Rename COLUMN "Number of Screens" To "screens";
+-- ALTER TABLE BolMovies Rename COLUMN "Revenue(INR)" To "revenue";
+-- ALTER TABLE BolMovies Rename COLUMN "Budget(INR)" To "budget";
+
+/*
+Check whether the movie was profitable or not
+*/
+
+SELECT movie , Actor, (budget-revenue) AS "Income" FROM BolMovies;
+
+SELECT movie , Actor, abs(budget-revenue) AS "Income" FROM BolMovies;
+
+
+
+SELECT movie , Actor, round(revenue/7) AS "IncomeInFloat" FROM BolMovies;
+SELECT movie , Actor, round((revenue/7),2) AS "IncomeInFloat" FROM BolMovies;
+
+/*Concatination*/
+
+SELECT movie, Concat(Actor,"-",Director) As "Crew" FROM BolMovies;
+SELECT movie, Concat(Actor,"-",Director,"-",revenue) As "Crew" FROM BolMovies;
+
+-- Lenght Function
+SELECT movie, length(movie) as "CharLength" FROM BolMovies;
+
+/*Substrings
+Indexing starts from 1
+*/
+-- substr(name,start,end)
+SELECT movie, substr(movie,4) as "Short" FROM BolMovies;
+SELECT movie, substr(movie,4,8) as "Short" FROM BolMovies;
+SELECT movie, substr(movie,1,8) as "Short" FROM BolMovies;
+
+/*Mathematical Operations*/
+-- Maximum;
+
+SELECT movie,max(budget) FROM BolMovies;
+
+-- Minimum
+SELECT movie,min(budget) FROM BolMovies;
+SELECT movie,min(revenue) FROM BolMovies;
+
+-- All indias Movies Gross Income
+
+SELECT sum(revenue) FROM BolMovies;
+-- In terms of Crore
+SELECT sum(revenue/10000000) As "InCrore" FROM BolMovies;
+SELECT round(sum(revenue/10000000)) As "InCrore" FROM BolMovies;
+
+/*Average Income of Movies in Bollywood*/
+
+SELECT avg(revenue) As "Average_Income" from BolMovies;
+
+SELECT round(avg(revenue),3) As "Average_Income" from BolMovies;
+SELECT round(avg(revenue/10000000),3) As "Average_Income_In_CR" from BolMovies;
+
+
+
