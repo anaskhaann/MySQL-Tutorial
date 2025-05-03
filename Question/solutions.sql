6704fc49d1b7b939b138c3776431535175386e19
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2025-05-03 15:44:36 +0530

    Add Solution to 2nd highest query

---
+++ Question/solutions.sql
@@ -0,0 +1,44 @@
+Create TABLE IF NOT EXISTS users (
+user_id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT
+);
+
+
+-- Q.1: To insert multiple values in database
+INSERT INTO users (user_id, name, age) VALUES 
+( 1 , "Anas", 21),
+(2, "Umar", 8),
+(3, "Salman", 54),
+(4, "Shahrukh", 60),
+(5, "Nazmul Hasan", 55);
+
+
+/*-- Q.2: To add/delete multiple cols in database, we can use multiple add commands but this is not working in sqlite*/
+
+ALTER TABLE users ADD email_id VARCHAR(25);
+
+-- TO delete the column
+ALTER TABLE users DROP email_id;
+
+
+/*Q.3 - 2nd highest VALUES from database(we are using movies database to solve this query*/
+
+SELECT * from movies;
+
+SELECT * FROM movies ORDER BY score DESC;
+
+SELECT * FROM movies ORDER BY score DESC LIMIT 5;
+
+SELECT * FROM movies ORDER BY score DESC LIMIT 1,5;
+
+-- Final result
+SELECT * FROM movies ORDER BY score DESC LIMIT 1,1;
+
+-- 2nd Lowest
+SELECT * FROM movies ORDER BY score LIMIT 5;
+
+SELECT * FROM movies ORDER BY score LIMIT 1,1;
+
+
+
